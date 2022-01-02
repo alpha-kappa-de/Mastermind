57bcca4774a32cd949f8cf38fce05eaf49f33cc1
α-kappa <87945287+alpha-kappa-de@users.noreply.github.com>
2022-01-02 21:22:45 +0100

    corrected the .bat file
    Corrected the "Compile Tests for Mastermind.bat"

--- Compile Tests for Mastermind.bat
+++ Compile Tests for Mastermind.bat
@@ -7,12 +7,29 @@ echo     /                     by alpha-kappa-de  /
 echo    /________________________________________/
 echo.
 echo This program will compile the translation units and your test using gcc und C89 standard. Then it will start the compiled test.
-echo Place it into the same path where your c files are located.
+echo Place it into the the root directory. (the directory with \code and \tests)
 echo.
 
-echo What's the name of your test? Example "TEST_mytest" (Without quotationmarks and without the ".c")
+echo What's the name of your test? Press TAB to scroll trough the folder or type in the name. 
+echo Example "TEST_mytest.c" (Without quotationmarks)
 echo.
+
+
+mkdir temp
+>NUL copy "code\mastermind.c" "temp\mastermind.c"
+>NUL copy "code\mastermind.h" "temp\mastermind.h"
+>NUL copy "code\mastermind_language.c" "temp\mastermind_language.c"
+>NUL copy "code\mastermind_language.h" "temp\mastermind_language.h"
+>NUL copy "code\mastermind_gui.c" "temp\mastermind_gui.c"
+>NUL copy "code\mastermind_gui.h" "temp\mastermind_gui.h"
+
+cd tests
 set /p test_file_name=
+
+cd ..\
+>NUL copy "tests\%test_file_name%" "temp\%test_file_name%"
+
+cd temp
 
 echo ___________________________________________________________________________
 echo Compiling mastermind.c
@@ -27,22 +44,29 @@ echo Compiling mastermind_gui.c
 cmd /c "gcc -c mastermind_gui.c mastermind_gui.h -ansi -pedantic -Wall -Wextra" 
 
 echo ___________________________________________________________________________
-echo Compiling %test_file_name%.c
-cmd /c "gcc mastermind.o mastermind_language.o mastermind_gui.o %test_file_name%.c -o %test_file_name%.exe -ansi -pedantic -Wall -Wextra" 
-
-
+echo Compiling %test_file_name%
+cmd /c "gcc mastermind.o mastermind_language.o mastermind_gui.o %test_file_name% -o %test_file_name%.exe -ansi -pedantic -Wall -Wextra" 
 
 echo ___________________________________________________________________________
-echo If you didn't see any warnings or errors then everything worked perfectly. 
-echo Else correct your code and try again.
+echo If you didn't see any warnings or errors then everything worked perfectly. Else correct your code and try again.
 echo.
+
+cd ..\
+>NUL copy "temp\%test_file_name%.exe" "mytest.exe"
+
+echo Cleaning up temporary files...
+rmdir "temp" /S /Q
+
 echo To run your test, press any key.
 pause >nul
 echo.
 
-cmd /c "%test_file_name%.exe"
+cmd /c "mytest.exe"
 
 echo.
 echo.
-echo End of test. Press any button to exit
+echo End of test. Press any button to clean up and exit.
 pause >nul
+
+del "mytest.exe"
+
