5f6290e2aeededdec71bf8acfbdf55bdb4815353
α-kappa <87945287+alpha-kappa-de@users.noreply.github.com>
2022-01-04 16:10:25 +0100

    moved scripts to a new folder called "scripts"

--- scripts/Compile Mastermind_testbuild.bat
+++ scripts/Compile Mastermind_testbuild.bat
@@ -2,24 +2,25 @@
 
 
 echo       _________________________________________
-echo      /     Compile Mastermind (for testing)   /
+echo      /       Compile Mastermind_testbuild     /
 echo     /                     by alpha-kappa-de  /
 echo    /________________________________________/
 echo.
 echo This program will build the game Mastermind and then delete it again. It is for testing purposes only.
-echo Place it into the the root directory. (the directory with \code and \tests)
+echo RUN THIS SCRIPT ONLY IN THE "/scripts" FOLDER!
 echo.
 
-mkdir temp
->NUL copy "code\mastermind.c" "temp\mastermind.c"
->NUL copy "code\mastermind.h" "temp\mastermind.h"
->NUL copy "code\mastermind_language.c" "temp\mastermind_language.c"
->NUL copy "code\mastermind_language.h" "temp\mastermind_language.h"
->NUL copy "code\mastermind_gui.c" "temp\mastermind_gui.c"
->NUL copy "code\mastermind_gui.h" "temp\mastermind_gui.h"
->NUL copy "code\program.c" "temp\program.c"
+cd ..\
+mkdir .temp
+>NUL copy "code\mastermind.c" ".temp\mastermind.c"
+>NUL copy "code\mastermind.h" ".temp\mastermind.h"
+>NUL copy "code\mastermind_language.c" ".temp\mastermind_language.c"
+>NUL copy "code\mastermind_language.h" ".temp\mastermind_language.h"
+>NUL copy "code\mastermind_gui.c" ".temp\mastermind_gui.c"
+>NUL copy "code\mastermind_gui.h" ".temp\mastermind_gui.h"
+>NUL copy "code\program.c" ".temp\program.c"
 
-cd temp
+cd .temp
 
 echo ___________________________________________________________________________
 echo Compiling mastermind.c
@@ -35,18 +36,12 @@ cmd /c "gcc -c mastermind_gui.c mastermind_gui.h -ansi -pedantic -Wall -Wextra"
 
 echo ___________________________________________________________________________
 echo Compiling program.c
-cmd /c "gcc mastermind.o mastermind_language.o mastermind_gui.o program.c -o Mastermind.exe -ansi -pedantic -Wall -Wextra" 
+cmd /c "gcc mastermind.o mastermind_language.o mastermind_gui.o program.c -o Mastermind_testbuild.exe -ansi -pedantic -Wall -Wextra" 
 
 echo ___________________________________________________________________________
 echo If you didn't see any warnings or errors then everything worked perfectly. 
 echo You can start the Game by typing Mastermind.exe into the command line.
 echo.
-
-cd ..\
->NUL copy "temp\Mastermind.exe" "Mastermind_testbuild.exe"
-
-echo Cleaning up temporary files...
-rmdir "temp" /S /Q
 
 echo To run your test, press any key.
 pause >nul
@@ -58,6 +53,7 @@ echo.
 echo.
 echo End of test. Press any button to clean up and exit.
 pause >nul
-
-del "Mastermind_testbuild.exe"
+echo Cleaning up temporary files...
+cd ..\
+rmdir ".temp" /S /Q
 
--- scripts/Compile Tests for Mastermind.bat
+++ scripts/Compile Tests for Mastermind.bat
@@ -7,29 +7,29 @@ echo     /                     by alpha-kappa-de  /
 echo    /________________________________________/
 echo.
 echo This program will compile the translation units and your test using gcc und C89 standard. Then it will start the compiled test.
-echo Place it into the the root directory. (the directory with \code and \tests)
+echo RUN THIS SCRIPT ONLY IN THE "/scripts" FOLDER!
 echo.
 
 echo What's the name of your test? Press TAB to scroll trough the folder or type in the name. 
 echo Example "TEST_mytest.c" (Without quotationmarks)
 echo.
 
-
-mkdir temp
->NUL copy "code\mastermind.c" "temp\mastermind.c"
->NUL copy "code\mastermind.h" "temp\mastermind.h"
->NUL copy "code\mastermind_language.c" "temp\mastermind_language.c"
->NUL copy "code\mastermind_language.h" "temp\mastermind_language.h"
->NUL copy "code\mastermind_gui.c" "temp\mastermind_gui.c"
->NUL copy "code\mastermind_gui.h" "temp\mastermind_gui.h"
+cd ..\
+mkdir .temp
+>NUL copy "code\mastermind.c" ".temp\mastermind.c"
+>NUL copy "code\mastermind.h" ".temp\mastermind.h"
+>NUL copy "code\mastermind_language.c" ".temp\mastermind_language.c"
+>NUL copy "code\mastermind_language.h" ".temp\mastermind_language.h"
+>NUL copy "code\mastermind_gui.c" ".temp\mastermind_gui.c"
+>NUL copy "code\mastermind_gui.h" ".temp\mastermind_gui.h"
 
 cd tests
 set /p test_file_name=
 
 cd ..\
->NUL copy "tests\%test_file_name%" "temp\%test_file_name%"
+>NUL copy "tests\%test_file_name%" ".temp\%test_file_name%"
 
-cd temp
+cd .temp
 
 echo ___________________________________________________________________________
 echo Compiling mastermind.c
@@ -51,22 +51,17 @@ echo ___________________________________________________________________________
 echo If you didn't see any warnings or errors then everything worked perfectly. Else correct your code and try again.
 echo.
 
-cd ..\
->NUL copy "temp\%test_file_name%.exe" "mytest.exe"
-
-echo Cleaning up temporary files...
-rmdir "temp" /S /Q
-
 echo To run your test, press any key.
 pause >nul
 echo.
 
-cmd /c "mytest.exe"
+cmd /c "%test_file_name%.exe"
 
 echo.
 echo.
 echo End of test. Press any button to clean up and exit.
 pause >nul
-
-del "mytest.exe"
+echo Cleaning up temporary files...
+cd ..\
+rmdir ".temp" /S /Q
 
