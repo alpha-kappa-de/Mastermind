134d69de57ac6aa3b6af6878eb262a37bf8cec60
α-kappa <87945287+alpha-kappa-de@users.noreply.github.com>
2022-01-07 18:40:34 +0100

    added ctype.h

--- code/mastermind.c
+++ code/mastermind.c
@@ -180,10 +180,11 @@ void language_menu()
 int player_guess_input(){
 	int i;
 	char *player_guess_char = malloc((colorcode_length) * 8 * sizeof(char));
-	tolower(scanf("%s",player_guess_char));
-	player_guess[0] = lang_color_name_to_number(strtok(player_guess_char,","));
-	for( i = 1 ; i < colorcode_length; ++i){
-		player_guess[i] = lang_color_name_to_number(strtok(player_guess_char,","));
+
+	tolower(scanf("%s", player_guess_char));
+	player_guess[0] = lang_color_name_to_number(strtok(player_guess_char, ","));
+	for( i = 1 ; i < colorcode_length; ++i) {
+		player_guess[i] = lang_color_name_to_number(strtok(player_guess_char, ","));
 	}
 	free(player_guess_char);
 	return 0;
