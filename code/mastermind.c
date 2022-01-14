b48483de3c5ac0c8f1fae60fc9078dfe8fb10e5c
Jooosy <96911451+Jooosy@users.noreply.github.com>
2022-01-14 14:53:21 +0100

    eingabefehler und reservierungsfehler berücksichtigen

--- code/mastermind.c
+++ code/mastermind.c
@@ -230,8 +230,9 @@ int player_guess_input()
 {
 	int i;
 	char *player_guess_char = malloc((colorcode_length) * 8 * sizeof(char));
-
+	// checken ob NULL zurückgegeben wird
 	tolower(scanf("%s", player_guess_char));
+	// einngabefehler berücksichtigen
 	player_guess[0] = lang_color_name_to_number(strtok(player_guess_char, ","));
 	for( i = 1 ; i < colorcode_length; ++i) {
 		player_guess[i] = lang_color_name_to_number(strtok(player_guess_char, ","));
